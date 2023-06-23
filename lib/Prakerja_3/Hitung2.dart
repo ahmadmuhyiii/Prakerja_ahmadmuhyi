c3c8786f94d0d384822a5f7a79ec804830cb6bf4
ahmadmuhyiii <ahmadmuhyi434@gmail.com>
2023-06-23 13:08:46 +0700

    folder 2 dan 3

---
+++ lib/Prakerja_3/Hitung2.dart
@@ -0,0 +1,14 @@
+void main() {
+  var sisi = 5;
+  var keliling = sisi * 4;
+  print('Keliling Persegi = $keliling');
+
+  var panjang = 10;
+  var lebar = 5;
+  var kel = 2 * (panjang + lebar);
+  print('keliling persegi panjang = $kel');
+
+  var diameter = 20;
+  var kelilinglingkaran = 3.14 * diameter;
+  print('keliling lingkaran = $kelilinglingkaran');
+}
